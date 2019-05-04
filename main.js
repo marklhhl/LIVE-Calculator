6ea5aeabd74aa886ec57dbfa8db5b86ae95bb6d4
marklhhl <marklhhl1@gmail.com>
2019-05-04 04:14:00 -0400

    completed calculation and corresponding display

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+// equal button
+var equal = document.getElementById("equal");
+
+// operator click listener
+equal.addEventListener('click', function() {
+    equalClickUpdate();
+});
+
 // operator buttons
 var plus = document.getElementById("plus");
 var minus = document.getElementById("minus");
 var divide = document.getElementById("divide");
 var multiply = document.getElementById("multiply");
+
+
+// operator click listener
+plus.addEventListener('click', function() {
+    operatorClickUpdate('+');
+});
+minus.addEventListener('click', function() {
+    operatorClickUpdate('-');
+});
+divide.addEventListener('click', function() {
+    operatorClickUpdate('/');
+});
+multiply.addEventListener('click', function() {
+    operatorClickUpdate('*');
+});
 
 
 
@@ -36,33 +59,43 @@ var nine = document.getElementById("nine");
 
 // click listeners for number buttons
 zero.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(zero.innerText);
 });
 one.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(one.innerText);
 });
 two.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(two.innerText);
 });
 three.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(three.innerText);
 });
 four.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(four.innerText);
 });
 five.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(five.innerText);
 });
 six.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(six.innerText);
 });
 seven.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(seven.innerText);
 });
 eight.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(eight.innerText);
 });
 nine.addEventListener('click', function() {
+    updateEquation();
     numberClickUpdate(nine.innerText);
 });
 
@@ -71,14 +104,22 @@ var dot = document.getElementById("dot");
 
 // dot click listener
 dot.addEventListener('click', function() {
+    updateEquation();
     dotClickUpdate();
 });
+
 
 // input display element
 var displayValElement = document.getElementById("calc-inputt");
 // variable that has the value of the input display
 var displayVal = '';
-// operator
+// current operator
+var curOperator = '';
+// string representation of equaltion
+var equation = '';
+// variable that indicates if previous button clicked was equal
+var equalClickedPrev = false;
+
 
 
 // update function for whenever a number button is clicked
@@ -93,7 +134,7 @@ function numberClickUpdate(num) {
 // function to handle dot button clicks
 function dotClickUpdate() {
     // do not append num to display input if the value of display input is empty and num is 0
-    if(!displayValElement.value.includes('.')){
+    if(!displayVal.includes('.')){
         displayVal += '.';
         displayValElement.value = displayVal;
     }   
@@ -103,6 +144,9 @@ function dotClickUpdate() {
 function clearClickUpdate() {
     displayVal = '';
     displayValElement.value = displayVal;
+    equation = '';
+    equalCLickedPrev = false;
+    curOperator = '';
 }
 
 // function to handle plus or minus button clicks
@@ -117,5 +161,40 @@ function plusOrMinusClickUpdate() {
             displayVal = '-' + displayVal
         }
         displayValElement.value = displayVal;
+    }
+}
+
+
+// function to handle operator button clicks
+function operatorClickUpdate(operator){
+    equalClickedPrev = false;
+    if (displayVal != ''){
+        curOperator = operator;
+    }
+}
+
+// function for updating equation when a number button is clicked after operator button
+function updateEquation(){
+    // clear if previous button clicked is equal
+    if (equalClickedPrev){
+        clearClickUpdate();
+    // update equation if previous button clicked was an operator
+    } else if (curOperator != ''){
+        equation = equation+displayVal+curOperator;
+        curOperator = '';
+        displayVal = '';
+    } 
+}
+
+// function for handling equal button click
+function equalClickUpdate(){
+    // display new value, set equalCLikedPrev to true, and reset variables
+    if (equation != ''){
+        equation = equation+displayVal;
+        displayVal = eval(equation);
+        displayValElement.value = displayVal;
+        equation = '';
+        curOperator = '';
+        equalClickedPrev = true;
     }
 }
